f62b97f9883e17e9aa99e20c0ce3e674567a3562
Bhavya-Fanshawe <bhavyathakkar396@gmail.com>
2022-09-13 14:48:53 -0400

    Lost files recovered

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+
+  function openService(All) {
+    var i;
+    var x = document.getElementsByClassName("All");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(ServiceName).style.display = "block";
+  }
+  
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".menu");
 
